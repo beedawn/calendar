df14b95f34ffc135f7c456a68f53423828c86be8
Bee Schmersal <brandonschmersal@gmail.com>
2025-01-27 18:38:59 -0500

    basic backend

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request){
+
+	fmt.Fprintf(w,"hello\n")
+}
+
+func main() {
+	fmt.Println("test")
+	http.HandleFunc("/hello", hello)
+
+	http.ListenAndServe(":8080", nil)
+
+} 
