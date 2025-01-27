5de27fd5ae13ecbd532f3a7cee6b0fc0808bf9a5
Bee Schmersal <brandonschmersal@gmail.com>
2025-01-27 18:47:42 -0500

    add post and get

--- backend/main.go
+++ backend/main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html"
 )
 
-func hello(w http.ResponseWriter, req *http.Request){
+func hello(w http.ResponseWriter, r *http.Request){
 
-	fmt.Fprintf(w,"hello\n")
+	fmt.Fprintf(w,"hello, %q\n", html.EscapeString(r.URL.Path))
+}
+
+
+func post (w http.ResponseWriter, r *http.Request){
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
 }
 
 func main() {
 	fmt.Println("test")
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/post", post)
 
 	http.ListenAndServe(":8080", nil)
 
