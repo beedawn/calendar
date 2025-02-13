49dfc6b52c0c407b26f5c4778a13d0c4c448a25a
Bee Schmersal <brandonschmersal@gmail.com>
2025-02-12 19:59:14 -0500

    move main

--- backend/main.go
+++ backend/main.go
@@ -1,123 +1,50 @@
 package main
 
 import (
-	"encoding/json"
+	"backend/calendar"
+	"backend/event"
+	"backend/resource"
 	"fmt"
-	"html"
-	"io"
-	"net/http"
-	"os"
+	"time"
 )
 
-import Calendar "calendar"
-
-func hello(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Fprintf(w, "hello, %q\n", html.EscapeString(r.URL.Path))
+func newLine(s string) string {
+	return s + "\n"
 }
 
-func post(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
+func loopEvents(c calendar.Calendar) {
+	for _, resource := range c.Resource {
+		for _, event := range resource.Event {
+
+			fmt.Println(event) // Or fmt.Printf("%+v\n", event) for more detail
+		}
+		fmt.Printf("Resource ID: %d\n", resource.Id)
 	}
 
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-		return
-	}
-	defer r.Body.Close()
-
-	fmt.Printf("Received POST request with body: %s\n", string(body))
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("POST request received sucesssfully"))
-}
-
-type Response struct {
-	Message string `json:"message"`
-	Status  int    `json:"status"`
-}
-
-func getJson(w http.ResponseWriter, r *http.Request) {
-	response := Response{
-		Message: "This is a json response",
-		Status:  200,
-	}
-	w.Header().Set("Content-Type", "application/json")
-
-	w.WriteHeader(http.StatusOK)
-
-	if err := json.NewEncoder(w).Encode(response); err != nil {
-		http.Error(w, "Failed ot encode JSON response", http.StatusInternalServerError)
-		return
-	}
-}
-
-type RequestData struct {
-	Name     string `json:"name"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
-	Age      int    `json:"age"`
-}
-
-func postJson(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read requestbody", http.StatusInternalServerError)
-		return
-	}
-	defer r.Body.Close()
-
-	var requestData RequestData
-	if err := json.Unmarshal(body, &requestData); err != nil {
-		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
-		return
-	}
-
-	response := Response{}
-	password := os.Getenv("PASSWORD")
-	if requestData.Name == "Bob" && requestData.Password == password {
-		fmt.Printf("Success\n")
-		response.Message = fmt.Sprintf("Secret")
-		response.Status = http.StatusOK
-	} else {
-		response.Message = "Invalid credentials"
-		response.Status = http.StatusUnauthorized
-
-	}
-
-	fmt.Printf("Received JSON: %+v\n", requestData)
-	fmt.Printf("%s\n", requestData.Name)
-	fmt.Printf("%d\n", requestData.Age)
-	fmt.Printf("%s\n", requestData.Email)
-
-	w.Header().Set("Content-Type", "application/json")
-
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(response); err != nil {
-		http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
-	}
 }
 
 func main() {
 
-	/*	http.HandleFunc("/hello", hello)
-		http.HandleFunc("/post", post)
-		http.HandleFunc("/getJson", getJson)
-		http.HandleFunc("/postJson", postJson)
-		http.ListenAndServe(":8080", nil)*/
-
 	var cal calendar.Calendar
-	cal.Test = "hi"
 
-	fmt.Printf(cal.test)
+	cal.Resource = make([]resource.Resource, 0)
 
+	newResource := resource.Resource{
+		Event: []event.Event{
+			{Id: 0, User: "user", StartTime: time.Now(), EndTime: time.Now(), CreatedTime: time.Now()},
+		},
+	}
+	//	newResource.Event = make([]event.Event, 0)
+	//	newResource.Event = append(newResource.Event, event.Event{0, "starttime", "user", "duration"})
+	cal.Resource = append(cal.Resource, newResource)
+	cal.NewResource()
+	startTime:=time.Now()
+	endTime:=time.Now()
+	cal.Resource[1].NewEvent(startTime, endTime)
+	cal.Resource[1].NewEvent(time.Now(), time.Now())
+
+
+	//loopEvents(cal)
+	cal.Resource[1].DeleteEvent(event.Event{})
+//	loopEvents(cal)
 }
